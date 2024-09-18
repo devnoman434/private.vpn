12ec6c8a993a2309f5cad113d87bb72c43f673e6
nomanqamar434 <noman@galixo.ai>
2024-09-18 15:37:04 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementsByClassName('head')[0].innerHTML= 'PRIVATE VPN is the best';
+document.getElementsByClassName("ye")[0].style.color = "white";
+document.getElementsByClassName("whi")[0].style.color = "yellow";
